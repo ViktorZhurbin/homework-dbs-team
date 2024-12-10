886c28ea67d370323e3615eba4c4bde11bfe157e
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-12-10 22:12:27 +0100

    add total stars and forks

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,12 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "./header.module.css";
 import logo from "./../../gitHub-mark-64px.png";
 import { fetchRepos, setSelectedUserName } from "../../store/actions/repos";
-import { getSelectedUserName } from "../../store/selectors/selectors";
+import { getRepoList, getSelectedUserName } from "../../store/selectors/selectors";
 
 const Header = () => {
   const dispatch = useDispatch();
 
   const selectedUserName = useSelector(getSelectedUserName);
+  const repos = useSelector(getRepoList);
+
+  const { stars, forks } = repos.reduce((acc, repo) => {
+    acc.stars += repo.watchers;
+    acc.forks += repo.forks;
+    return acc;
+  }, { stars: 0, forks: 0 });
 
   useEffect(() => {
     if (selectedUserName) dispatch(fetchRepos());
@@ -36,17 +43,23 @@ const Header = () => {
   return (
     <div className={styles.header}>
       <img src={logo} alt="logo" />
-      <div className={styles.menu}>
-        <input
-          type="text"
-          value={selectedUserName}
-          placeholder="Enter GitHub user name..."
-          onChange={handleSearchTextChange}
-          onKeyUp={handleKeyUp}
-        />
-        <button onClick={handleButtonClick} disabled={selectedUserName === ""}>
-          <i className={`fas fa-search ${styles.icon}`}></i> List repos
-        </button>
+      <div className={styles.content}>
+        <div className={styles.menu}>
+          <input
+            type="text"
+            value={selectedUserName}
+            placeholder="Enter GitHub user name..."
+            onChange={handleSearchTextChange}
+            onKeyUp={handleKeyUp}
+          />
+          <button onClick={handleButtonClick} disabled={selectedUserName === ""}>
+            <i className={`fas fa-search ${styles.icon}`}></i> List repos
+          </button>
+        </div>
+        <div className={styles.stats}>
+          <b>Stars: {stars},</b>
+          <b>Forks: {forks}</b>
+        </div>
       </div>
     </div>
   );
